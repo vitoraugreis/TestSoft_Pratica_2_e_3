f11620366c3710f354c637a9b7008405286dee04
vitoraugreis <vitoraugreis@gmail.com>
2025-09-28 19:32:21 -0300

    3. Classe TestResult

--- testing_framework.py
+++ testing_framework.py
@@ -2,17 +2,49 @@ class TestCase:
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
 
-    def run(self):
-        self.set_up()    # chama método de setup
-        test_method = getattr(self, self.test_method_name)
-        test_method()    # chama método de teste 
-        self.tear_down() # chama método de teardown 
+    def run(self, result):
+        result.test_started()
+        self.set_up()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name)
+        self.tear_down()
 
     def set_up(self):
         pass
 
     def tear_down(self):
         pass
+
+class TestResult:
+    RUN_MSG = 'run'
+    FAILURE_MSG = 'failed'
+    ERROR_MSG = 'error'
+
+    def __init__(self, suite_name=None):
+        self.run_count = 0
+        self.failures = []
+        self.errors = []
+
+    def test_started(self):
+        self.run_count += 1
+
+    def add_failure(self, test):
+        self.failures.append(test)
+
+    def add_error(self, test):
+        self.errors.append(test)
+
+    def summary(self):
+        return f'{'='*35}\n' \
+               f'{self.run_count} {self.RUN_MSG}, ' \
+               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
+               f'{str(len(self.errors))} {self.ERROR_MSG}' \
+               f'\n{'='*35}'
 
 class MyTest(TestCase):
     def set_up(self):
@@ -31,14 +63,18 @@ class MyTest(TestCase):
         print('test_c')
 
 def main():
+    result = TestResult()
+
     test = MyTest('test_a')
-    test.run()
+    test.run(result)
 
     test = MyTest('test_b')
-    test.run()
+    test.run(result)
 
     test = MyTest('test_c')
-    test.run()
+    test.run(result)
+
+    print(result.summary())
 
 if __name__ == "__main__":
     main()
