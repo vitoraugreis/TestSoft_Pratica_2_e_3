05caf4ad565055a645b38cf905c0a9b3642ccb22
vitoraugreis <vitoraugreis@gmail.com>
2025-09-29 19:20:47 -0300

    6. Classes TestLoader e TestRunner

--- testing_framework.py
+++ testing_framework.py
@@ -55,6 +55,31 @@ class TestSuite:
         for test in self.tests:
             test.run(result)
 
+class TestLoader:
+    TEST_METHOD_PREFIX = 'test'
+
+    def get_test_case_names(self, test_case_class):
+        methods = dir(test_case_class)
+        test_method_names = list(filter(lambda method: 
+            method.startswith(self.TEST_METHOD_PREFIX), methods))
+        return test_method_names
+
+    def make_suite(self, test_case_class):
+        suite = TestSuite()
+        for test_method_name in self.get_test_case_names(test_case_class):
+            test_method = test_case_class(test_method_name)
+            suite.add_test(test_method)
+        return suite
+
+class TestRunner:
+    def __init__(self):
+        self.result = TestResult()
+
+    def run(self, test):
+        test.run(self.result)
+        print(self.result.summary())
+        return self.result
+
 class TestStub(TestCase):
     def test_success(self):
         assert True
@@ -163,25 +188,67 @@ class TestSuiteTest(TestCase):
 
         assert result.summary() == '3 run, 1 failed, 1 error'    
 
+class TestLoaderTest(TestCase):
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
+        assert len(suite.tests) == 3
+
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
+
+        suite = TestSuite()
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
+
+        assert len(suite.tests) == 2
+
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert names == ['test_error', 'test_failure', 'test_success']
+
+    def test_get_no_test_case_names(self):
+        class Test(TestCase):
+            def foobar(self):
+                pass
+
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
+
 def main():
-    result = TestResult()
-    suite = TestSuite()
+    # result = TestResult()
+    # suite = TestSuite()
     
-    suite.add_test(TestCaseTest('test_result_success_run'))
-    suite.add_test(TestCaseTest('test_result_failure_run'))
-    suite.add_test(TestCaseTest('test_result_error_run'))
-    suite.add_test(TestCaseTest('test_result_multiple_run'))
-    suite.add_test(TestCaseTest('test_was_set_up'))
-    suite.add_test(TestCaseTest('test_was_run'))
-    suite.add_test(TestCaseTest('test_was_tear_down'))
-    suite.add_test(TestCaseTest('test_template_method'))
+    # suite.add_test(TestCaseTest('test_result_success_run'))
+    # suite.add_test(TestCaseTest('test_result_failure_run'))
+    # suite.add_test(TestCaseTest('test_result_error_run'))
+    # suite.add_test(TestCaseTest('test_result_multiple_run'))
+    # suite.add_test(TestCaseTest('test_was_set_up'))
+    # suite.add_test(TestCaseTest('test_was_run'))
+    # suite.add_test(TestCaseTest('test_was_tear_down'))
+    # suite.add_test(TestCaseTest('test_template_method'))
     
-    suite.add_test(TestSuiteTest('test_suite_size'))
-    suite.add_test(TestSuiteTest('test_suite_success_run'))
-    suite.add_test(TestSuiteTest('test_suite_multiple_run'))
+    # suite.add_test(TestSuiteTest('test_suite_size'))
+    # suite.add_test(TestSuiteTest('test_suite_success_run'))
+    # suite.add_test(TestSuiteTest('test_suite_multiple_run'))
     
-    suite.run(result)
-    print(result.summary())
+    # suite.run(result)
+    # print(result.summary())
+    # result = TestResult()
+    # loader = TestLoader()
+    # suite = loader.make_suite(TestLoaderTest)
+    # suite.run(result)
+    # print(result.summary())
+    
+    loader = TestLoader()
+    suite = loader.make_suite(TestLoaderTest)
+
+    runner = TestRunner()
+    runner.run(suite)
 
 if __name__ == "__main__":
     main()
