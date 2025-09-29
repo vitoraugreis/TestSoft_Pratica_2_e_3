c4cc2fa7251f631ab449a6f32cdbceb2aa0046c0
vitoraugreis <vitoraugreis@gmail.com>
2025-09-29 19:42:45 -0300

    8. Comandos Assert

--- testing_framework.py
+++ testing_framework.py
@@ -20,6 +20,26 @@ class TestCase:
     def tear_down(self):
         pass
 
+    def assert_equal(self, first, second):
+        if first != second:
+            msg = f'{first} != {second}'
+            raise AssertionError(msg)
+        
+    def assert_true(self, expr):
+        if not expr:
+            msg = f'{expr} is not true'
+            raise AssertionError(msg)
+
+    def assert_false(self, expr):
+        if expr:
+            msg = f'{expr} is not false'
+            raise AssertionError(msg)
+
+    def assert_in(self, member, container):
+        if member not in container:
+            msg = f'{member} not found in {container}'
+            raise AssertionError(msg)
+
 class TestResult:
     RUN_MSG = 'run'
     FAILURE_MSG = 'failed'
@@ -117,17 +137,17 @@ class TestCaseTest(TestCase):
     def test_result_success_run(self):
         stub = TestStub('test_success')
         stub.run(self.result)
-        assert self.result.summary() == '1 run, 0 failed, 0 error'
+        self.assert_equal(self.result.summary(), '1 run, 0 failed, 0 error')
 
     def test_result_failure_run(self):
         stub = TestStub('test_failure')
         stub.run(self.result)
-        assert self.result.summary() == '1 run, 1 failed, 0 error'
+        self.assert_equal(self.result.summary(), '1 run, 1 failed, 0 error')
 
     def test_result_error_run(self):
         stub = TestStub('test_error')
         stub.run(self.result)
-        assert self.result.summary() == '1 run, 0 failed, 1 error'
+        self.assert_equal(self.result.summary(), '1 run, 0 failed, 1 error')
 
     def test_result_multiple_run(self):
         stub = TestStub('test_success')
@@ -136,27 +156,51 @@ class TestCaseTest(TestCase):
         stub.run(self.result)
         stub = TestStub('test_error')
         stub.run(self.result)
-        assert self.result.summary() == '3 run, 1 failed, 1 error'
+        self.assert_equal(self.result.summary(), '3 run, 1 failed, 1 error')
 
     def test_was_set_up(self):
         spy = TestSpy('test_method')
         spy.run(self.result)
-        assert spy.was_set_up
+        self.assert_true(spy.was_set_up)
 
     def test_was_run(self):
         spy = TestSpy('test_method')
         spy.run(self.result)
-        assert spy.was_run
+        self.assert_true(spy.was_run)
 
     def test_was_tear_down(self):
         spy = TestSpy('test_method')
         spy.run(self.result)
-        assert spy.was_tear_down
+        self.assert_true(spy.was_tear_down)
 
     def test_template_method(self):
         spy = TestSpy('test_method')
         spy.run(self.result)
-        assert spy.log == "set_up test_method tear_down"
+        self.assert_equal(spy.log, "set_up test_method tear_down")
+
+    def test_assert_true(self):
+        self.assert_true(True)
+
+    def test_assert_false(self):
+        self.assert_false(False)
+
+    def test_assert_equal(self):
+        self.assert_equal("", "")
+        self.assert_equal("foo", "foo")
+        self.assert_equal([], [])
+        self.assert_equal(['foo'], ['foo'])
+        self.assert_equal((), ())
+        self.assert_equal(('foo',), ('foo',))
+        self.assert_equal({}, {})
+        self.assert_equal({'foo'}, {'foo'})
+
+    def test_assert_in(self):
+        animals = {'monkey': 'banana', 'cow': 'grass', 'seal': 'fish'}
+
+        self.assert_in('a', 'abc')
+        self.assert_in('foo', ['foo'])
+        self.assert_in(1, [1, 2, 3])
+        self.assert_in('monkey', animals)
 
 class TestSuiteTest(TestCase):
     def test_suite_size(self):
@@ -166,7 +210,7 @@ class TestSuiteTest(TestCase):
         suite.add_test(TestStub('test_failure'))
         suite.add_test(TestStub('test_error'))
 
-        assert len(suite.tests) == 3
+        self.assert_equal(len(suite.tests), 3)
 
     def test_suite_success_run(self):
         result = TestResult()
@@ -175,7 +219,7 @@ class TestSuiteTest(TestCase):
 
         suite.run(result)
 
-        assert result.summary() == '1 run, 0 failed, 0 error'
+        self.assert_equal(result.summary(), '1 run, 0 failed, 0 error')
 
     def test_suite_multiple_run(self):
         result = TestResult()
@@ -186,13 +230,13 @@ class TestSuiteTest(TestCase):
 
         suite.run(result)
 
-        assert result.summary() == '3 run, 1 failed, 1 error'    
+        self.assert_equal(result.summary(), '3 run, 1 failed, 1 error')    
 
 class TestLoaderTest(TestCase):
     def test_create_suite(self):
         loader = TestLoader()
         suite = loader.make_suite(TestStub)
-        assert len(suite.tests) == 3
+        self.assert_equal(len(suite.tests), 3)
 
     def test_create_suite_of_suites(self):
         loader = TestLoader()
@@ -203,12 +247,12 @@ class TestLoaderTest(TestCase):
         suite.add_test(stub_suite)
         suite.add_test(spy_suite)
 
-        assert len(suite.tests) == 2
+        self.assert_equal(len(suite.tests), 2)
 
     def test_get_multiple_test_case_names(self):
         loader = TestLoader()
         names = loader.get_test_case_names(TestStub)
-        assert names == ['test_error', 'test_failure', 'test_success']
+        self.assert_equal(names, ['test_error', 'test_failure', 'test_success'])
 
     def test_get_no_test_case_names(self):
         class Test(TestCase):
@@ -217,7 +261,7 @@ class TestLoaderTest(TestCase):
 
         loader = TestLoader()
         names = loader.get_test_case_names(Test)
-        assert names == []
+        self.assert_equal(names, [])
 
 def main():
     loader = TestLoader()
